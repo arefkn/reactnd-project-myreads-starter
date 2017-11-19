df575fc1c913a97bba5180c9e37d9b540a2c622d
Aref <aref.kn@oracle.com>
2017-11-18 22:19:21 -0800

    Creating Shelf component

--- src/Book.js
+++ src/Book.js
@@ -1,30 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Book = ({ title, authors, coverUrl, currentShelve }) => {
+const Book = ({ title, authors, coverUrl, shelves, currentShelf }) => {
   return (
     <div className="book">
       <div className="book-top">
         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url('${coverUrl}')` }}></div>
         <div className="book-shelf-changer">
-          <select>
+          <select defaultValue={currentShelf}>
             <option value="none" disabled>Move to...</option>
-            <option value="currentlyReading">Currently Reading</option>
-            <option value="wantToRead">Want to Read</option>
-            <option value="read">Read</option>
-            <option value="none">None</option>
+            {shelves.map(({ value, label }) => <option key={value} value={value}>{label}</option>)}
           </select>
         </div>
       </div>
       <div className="book-title">{title}</div>
-      <div className="book-authors">{authors}</div>
+      <div className="book-authors">{authors.join(", ")}</div>
     </div>
   );
 };
 
 Book.propTypes = {
-  title: React.PropTypes.string.isRequired,
-  authors: React.PropTypes.string.isRequired,
-  coverUrl: React.PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  authors: PropTypes.arrayOf(PropTypes.string).isRequired,
+  coverUrl: PropTypes.string.isRequired,
+  shelves: PropTypes.arrayOf(PropTypes.object).isRequired,
+  currentShelf: PropTypes.string.isRequired,
 };
 
 export default Book;
